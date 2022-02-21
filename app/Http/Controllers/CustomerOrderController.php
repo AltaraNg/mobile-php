ab7c94effb97f8c4bf88f7319354418f93336e93
harmlessprince <realolamilekan@gmail>
2022-02-21 20:56:46 +0100

    setting up array structure

--- app/Http/Controllers/CustomerOrderController.php
+++ app/Http/Controllers/CustomerOrderController.php
@@ -2,9 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreOrderRequest;
 use App\Http\Resources\CustomerResource;
 use App\Models\Customer;
 use App\Repositories\Eloquent\Repository\CustomerRepository;
+use App\Services\GoogleSheetService;
+use Carbon\Carbon;
+use Illuminate\Support\Arr;
 
 /**
  * @group Customer Order
@@ -15,10 +19,12 @@ use App\Repositories\Eloquent\Repository\CustomerRepository;
 class CustomerOrderController extends Controller
 {
     private $customerRepository;
+    private $googleSheetService;
 
-    public function __construct(CustomerRepository $customerRepository)
+    public function __construct(CustomerRepository $customerRepository, GoogleSheetService $googleSheetService)
     {
         $this->customerRepository = $customerRepository;
+        $this->googleSheetService = $googleSheetService;
     }
 
     /**
@@ -31,5 +37,23 @@ class CustomerOrderController extends Controller
     {
         $customer = $customer->load('orders');
         return $this->sendSuccess([new CustomerResource($customer)], 'Customer orders successfully fetched');
+    }
+
+
+    public function submitRequest(string $documentId)
+    {
+        $customer = auth()->user();
+        $input = [
+            'customer_id' => $customer->id,
+            'name' => $customer->first_name . ' ' . $customer->last_name,
+            'phone_number' => $customer->telephone,
+            'order_type' => $request->order_t
+            'request_date' => Carbon::now()->format('Y-m-d')
+        ];
+        $data = [
+            array_values($input)
+        ];
+        dd($data);
+        $this->googleSheetService->appendSheet($data, $documentId);
     }
 }

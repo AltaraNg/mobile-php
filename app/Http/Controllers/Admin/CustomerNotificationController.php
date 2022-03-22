e49945cc92a153181f409de4e0b9b45207f44756
harmlessprince <realolamilekan@gmail>
2022-03-22 10:33:58 +0100

    updated mobile notification controller

--- app/Http/Controllers/Admin/CustomerNotificationController.php
+++ app/Http/Controllers/Admin/CustomerNotificationController.php
@@ -27,7 +27,7 @@ class CustomerNotificationController extends Controller
      */
     public function show(Customer $customer)
     {
-        $notifications = DB::table('notifications')->where('notifiable_type', 'App\Customer')->where('type', 'App\Notifications\CustomerNotification')->where('notifiable_id', $customer->id)->latest()->paginate(15);
+        $notifications = DB::table('notifications')->where('notifiable_type', 'App\Customer')->where('type', 'App\Notifications\CustomerMobileMessageNotification')->where('notifiable_id', $customer->id)->latest()->paginate(15);
         return $this->sendSuccess(['notifications' => $notifications], 'User notifications retrieved');
     }
 }

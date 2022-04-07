1211a36550d5d6ba2db05b0b76b50cc5aafd4873
harmlessprince <realolamilekan@gmail>
2022-04-07 10:20:17 +0100

    removed expiriation of otp and token

--- app/Http/Controllers/OtpController.php
+++ app/Http/Controllers/OtpController.php
@@ -35,15 +35,16 @@ class OtpController extends Controller
     public function sendOtp(SendOtpRequest $request)
     {
         $otp = $this->otpService->generate($request->phone_number);
-        if (!$otp->status) {
-            return $this->sendError($otp->message, HttpResponseCodes::REQUEST_NOT_PERMITTED);
-        }
-        $message = $otp->otp . " is your Altara app login code and it expires in " . $otp->expires_in;
+       
+        // if (!$otp->status) {
+        //     return $this->sendError($otp->message, HttpResponseCodes::REQUEST_NOT_PERMITTED);
+        // }
+        // $message = $otp->otp . " is your Altara app login code and it expires in " . $otp->expires_in;
         $response = $this->messageService->sendMessage($request->phone_number, $message);
         $messageStatus = $response->messages[0]->status;
         if ($messageStatus->groupName != 'Success') {
             return $this->sendError($messageStatus->description, HttpResponseCodes::ACTION_FAILED);
         }
-        return $this->sendSuccess([], 'Otp has been successfully sent' . $messageStatus->groupName);
+        return $this->sendSuccess([], $messageStatus->description);
     }
 }

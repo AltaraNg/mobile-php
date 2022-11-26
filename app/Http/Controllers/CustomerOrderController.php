552bd3528eb46c4237396d227ce4e779f2bc894d
Omodamola Oladeji <darmhoo@hotmail.com>
2022-11-26 05:36:57 +0000

    fix order request additions

--- app/Http/Controllers/CustomerOrderController.php
+++ app/Http/Controllers/CustomerOrderController.php
@@ -91,6 +91,8 @@ class CustomerOrderController extends Controller
             $orderRequest = OrderRequest::create([
                 'customer_id' => $customer->id,
                 'order_type' => $request->order_type,
+                'branch' => $request->branch,
+                'staff_name' => $request->staff_name,
                 'request_date' => now(),
                 'status' => OrderRequest::STATUS_PENDING
             ]);

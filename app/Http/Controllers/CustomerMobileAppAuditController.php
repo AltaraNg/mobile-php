cfbf9d6a84ef9fe748f5870f4e7371f0a712facd
harmlessprince <realolamilekan@gmail>
2023-10-12 11:02:22 +0100

    ability to log customer activities on APP

--- app/Http/Controllers/CustomerMobileAppAuditController.php
+++ app/Http/Controllers/CustomerMobileAppAuditController.php
@@ -2,9 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreCustomerMobileAppAudit;
+use App\Repositories\Eloquent\Repository\CustomerMobileAppAuditRepository;
 use Illuminate\Http\Request;
 
 class CustomerMobileAppAuditController extends Controller
 {
-    //
+    private CustomerMobileAppAuditRepository $customerMobileAppAuditRepository;
+    public function __construct(CustomerMobileAppAuditRepository $customerMobileAppAuditRepository) {
+        $this->customerMobileAppAuditRepository = $customerMobileAppAuditRepository;
+    }
+
+    public function store(StoreCustomerMobileAppAudit $request)
+    {
+        $data = $request->validated() + [
+            'customer_id' => auth()->user()->id,
+        ];
+        $this->customerMobileAppAuditRepository->create($data);
+        return $this->sendSuccess([], "Audited successfully");
+    }
 }
